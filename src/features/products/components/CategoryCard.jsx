399a8ea82d23a0f44e7fd940e48c970dd3632a75
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-06-10 19:22:56 +0430

    style: refactor CategoryCard layout for improved structure and styling

--- src/features/products/components/CategoryCard.jsx
+++ src/features/products/components/CategoryCard.jsx
@@ -1,22 +1,25 @@
 export default function CategoryCard({ category }) {
   return (
     <div
-      className="rounded-2xl
-         bg-white
-        flex
-        flex-col
-        justify-between
+      className="
+        overflow-hidden
+        rounded-2xl
+        bg-white
         transition-all
         duration-300
         hover:-translate-y-1
-        hover:shadow-lg"
+        hover:shadow-lg
+      "
     >
-      <img
-        src={category.img}
-        alt={category.name}
-        className="w-full h-full object-cover"
-      />
-      <h3 className="p-4">{category.name}</h3>
+      <img src={category.img} alt={category.name} className="h-70 w-full" />
+
+      <div className="p-5">
+        <h3 className="text-lg font-semibold">{category.name}</h3>
+
+        <p className="mt-1 text-sm text-neutral-500">
+          {category.count} Products
+        </p>
+      </div>
     </div>
   );
 }
