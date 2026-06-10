40ccf08fb83058dc7c3c284cb846d7e994e5e86d
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-06-10 18:47:53 +0430

    style: remove onClick prop from CategoryCard and enhance styling for better visual appeal

--- src/features/products/components/CategoryCard.jsx
+++ src/features/products/components/CategoryCard.jsx
@@ -1,6 +1,18 @@
-export default function CategoryCard({ category, onClick }) {
+export default function CategoryCard({ category }) {
   return (
-    <div className="category-card" onClick={onClick}>
+    <div
+      className="rounded-2xl
+        border
+        p-4
+        flex
+        flex-col
+        justify-between
+        transition-all
+        duration-300
+        hover:-translate-y-1
+        hover:shadow-lg"
+    >
+      <img src={`/images/${category}-category.jpg`} alt={category} />
       <h3>{category}</h3>
     </div>
   );
