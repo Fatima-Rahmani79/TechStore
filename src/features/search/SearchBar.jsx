6f8408f43818db20e85bc4f5489a901cfd30df17
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-06-13 22:30:21 +0430

    style: enhance SearchBar component with improved layout and styling

--- src/features/search/SearchBar.jsx
+++ src/features/search/SearchBar.jsx
@@ -1,11 +1,19 @@
+import { Search } from "lucide-react";
+
 export default function SearchBar({ searchTerm, onSearchTerm }) {
   return (
-    <div>
+    <div className="relative mb-8">
+      <Search
+        size={18}
+        className="absolute top-1/2 left-4 -translate-y-1/2 text-neutral-400"
+      />
+
       <input
-        className="border-1"
         type="text"
+        placeholder="Search products..."
         value={searchTerm}
         onChange={(e) => onSearchTerm(e.target.value)}
+        className="w-full rounded-xl border border-neutral-300 py-3 pr-4 pl-11 outline-none transition focus:border-black"
       />
     </div>
   );
