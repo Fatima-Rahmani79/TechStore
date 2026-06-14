629ab678b54b514b29f290f84cdaffd8aca50dca
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-06-14 10:53:05 +0430

    style: implement SearchModal component in Navbar for enhanced search functionality; manage search state with useState

--- src/components/sections/SearchModal.jsx
+++ src/components/sections/SearchModal.jsx
@@ -1,8 +1,29 @@
+import { X } from "lucide-react";
 import { useSearch } from "../../context/search/useSearch";
 
-export default function SearchModal() {
+export default function SearchModal({ onClose }) {
   const { searchTerm, setSearchTerm } = useSearch();
+
   return (
-    <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+    <div className="fixed inset-0 z-50 bg-black/40">
+      <div className="mx-auto mt-24 max-w-md rounded-2xl bg-white p-6 shadow-xl">
+        <div className="mb-4 flex items-center justify-between">
+          <h2 className="font-semibold">Search Products</h2>
+
+          <button onClick={onClose}>
+            <X size={18} />
+          </button>
+        </div>
+
+        <input
+          type="text"
+          placeholder="Search products..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="w-full rounded-xl border px-4 py-3"
+          autoFocus
+        />
+      </div>
+    </div>
   );
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,42 +1,48 @@
 import { Link } from "react-router-dom";
 import { Search, Heart, ShoppingCart } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import SearchModal from "../components/sections/SearchModal";
+import { useState } from "react";
 
 export default function Navbar() {
-  // const { searchTerm, setSearchTerm } = useSearch();
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
   return (
-    <header className="sticky w-full top-0 z-50 border-b border-neutral-200 bg-white/80 backdrop-blur-md">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
-        <h1 className="text-xl font-bold">
-          <Link to="/" className="text-xl font-bold">
-            TechStore.
-          </Link>
-        </h1>
-
-        <ul className=" flex items-center space-x-6">
-          <li>
-            <NavLink to="/shop">Shop</NavLink>
-          </li>
-
-          <li>
-            <Link className="text-gray-700" to={"/about"}>
-              About
+    <>
+      <header className="sticky w-full top-0 z-50 border-b border-neutral-200 bg-white/80 backdrop-blur-md">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between px-6 py-4">
+          <h1 className="text-xl font-bold">
+            <Link to="/" className="text-xl font-bold">
+              TechStore.
             </Link>
-          </li>
-        </ul>
+          </h1>
 
-        <div className="flex items-center space-x-4">
-          <button className="btn-icon">
-            <Search />
-          </button>
-          <button className="btn-icon">
-            <Heart />
-          </button>
-          <button className="btn-icon">
-            <ShoppingCart />
-          </button>
-        </div>
-      </nav>
-    </header>
+          <ul className=" flex items-center space-x-6">
+            <li>
+              <NavLink to="/shop">Shop</NavLink>
+            </li>
+
+            <li>
+              <Link className="text-gray-700" to={"/about"}>
+                About
+              </Link>
+            </li>
+          </ul>
+
+          <div className="flex items-center space-x-4">
+            <button className="btn-icon" onClick={() => setIsSearchOpen(true)}>
+              <Search />
+            </button>
+            <button className="btn-icon">
+              <Heart />
+            </button>
+            <button className="btn-icon">
+              <ShoppingCart />
+            </button>
+          </div>
+        </nav>
+      </header>
+      {isSearchOpen && <SearchModal onClose={() => setIsSearchOpen(false)} />}
+    </>
   );
 }
