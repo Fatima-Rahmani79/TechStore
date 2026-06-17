5feb78b91b39f9df1cd19109888528c3a13998b3
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-06-17 22:23:16 +0430

    feat: implement cart slice for managing cart state and remove CartDrawer component

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+
+  reducers: {
+    addToCart(state, action) {
+      const product = action.payload;
+
+      const existingItem = state.items.find((item) => item.id === product.id);
+
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({
+          ...product,
+          quantity: 1,
+        });
+      }
+    },
+  },
+});
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,3 +1,3 @@
 export default function Cart() {
-  return <h1>Cart Page</h1>;
+  return <h1></h1>;
 }
