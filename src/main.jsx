db7216f5a37c40450323a39b42f75ee50be654fa
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-06-17 23:27:09 +0430

    feat: set up Redux store and integrate with main application

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/globals.css";
 import App from "./App";
 import { SearchProvider } from "./context/search/SearchContext";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <SearchProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </SearchProvider>
   </BrowserRouter>,
 );
