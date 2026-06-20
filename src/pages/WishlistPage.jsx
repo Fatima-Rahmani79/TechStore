f791825f13bbd785c69bf724720913e4b55effeb
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-06-20 23:51:38 +0430

    feat: implement WishlistItem component to display individual wishlist items

--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -1,3 +1,13 @@
 export default function WishlistItem({ item }) {
-  return <h1>Wishlist Page</h1>;
+  return (
+    <div>
+      <img src={item.images[0]} alt={item.name} />
+
+      <h3>{item.name}</h3>
+
+      <p>{item.price}</p>
+
+      <button>Remove</button>
+    </div>
+  );
 }
