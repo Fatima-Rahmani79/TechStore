71a60f3382abaf42bd5080e08621f902e5da76e3
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-06-27 17:18:53 +0430

    feat: add ScrollToTop component to App for improved user navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import AppRoutes from "./routes/AppRoutes";
 
 import { useDarkMode } from "./hooks/useDarkMode";
 import AiChatWidget from "./features/ai/components/AiChatWidget";
+import ScrollToTop from "./components/ui/ScrollToTop";
 
 export default function App() {
   useDarkMode(); // ← فقط همین کافیه، بقیه hook کارش رو می‌کنه
   return (
     <>
+      <ScrollToTop />
       <AppRoutes />
       <AiChatWidget />
     </>
